7f062072932b60be6fdd5ea990f00aa712a1d669
rakib <mehrabishraqhossian@gmail.com>
2024-09-27 19:40:13 +0600

    learn bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import Form from "./components/Form";
+import NavAndTab from "./components/NavAndTab";
 
-const App = () => {
-  const [userInfo, setUserInfo] = useState({
-    userName: "",
-    email: "",
-    password: "",
-  });
-
-  const [storedData, setStoredData] = useState(); // State to hold retrieved data
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    setUserInfo((prevInfo) => ({
-      ...prevInfo,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = () => {
-    console.log(userInfo);
-    localStorage.setItem("user", JSON.stringify(userInfo));
-  };
-
-  useEffect(() => {
-    const data = localStorage.getItem("user");
-
-    setStoredData(JSON.parse(data));
-  }, []);
-
+export const App = () => {
   return (
     <>
-      <h1>Local Storage</h1>
-      <input
-        type="text"
-        placeholder="username.."
-        className="form-control"
-        name="userName"
-        value={userInfo.userName}
-        onChange={handleChange}
-      />
-      <input
-        type="email"
-        placeholder="email.."
-        className="form-control"
-        name="email"
-        value={userInfo.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        placeholder="password.."
-        className="form-control"
-        name="password"
-        value={userInfo.password}
-        onChange={handleChange}
-      />
-
-      <button className="btn btn-info" onClick={handleSubmit}>
-        Register
-      </button>
-      <div>
-        <div>
-          <h2>Stored User Info:</h2>
-          <p>Username: {storedData?.userName}</p>
-          <p>Email: {storedData?.email}</p>
-          <p>Password: {storedData?.password}</p>
-        </div>
-      </div>
+      <Form />
+      <NavAndTab />
     </>
   );
 };
---
+++ src/components/Form.jsx
@@ -0,0 +1,133 @@
+import React, { useEffect } from "react";
+
+const Form = () => {
+  useEffect(() => {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll(".needs-validation");
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  }, []);
+
+  return (
+    <form className="row g-3 needs-validation" noValidate>
+      <div className="col-md-4">
+        <label htmlFor="validationCustom01" className="form-label">
+          First name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="validationCustom01"
+          required
+        />
+        <div className="valid-feedback">Looks good!</div>
+        <div className="invalid-feedback">First name is required.</div>
+      </div>
+      <div className="col-md-4">
+        <label htmlFor="validationCustom02" className="form-label">
+          Last name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="validationCustom02"
+          required
+        />
+        <div className="valid-feedback">Looks good!</div>
+        <div className="invalid-feedback">Last name is required.</div>
+      </div>
+      <div className="col-md-4">
+        <label htmlFor="validationCustomUsername" className="form-label">
+          Username
+        </label>
+        <div className="input-group has-validation">
+          <span className="input-group-text" id="inputGroupPrepend">
+            @
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            id="validationCustomUsername"
+            aria-describedby="inputGroupPrepend"
+            required
+          />
+          <div className="invalid-feedback">Please choose a username.</div>
+        </div>
+      </div>
+      <div className="col-md-6">
+        <label htmlFor="validationCustom03" className="form-label">
+          City
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="validationCustom03"
+          required
+        />
+        <div className="invalid-feedback">Please provide a valid city.</div>
+      </div>
+      <div className="col-md-3">
+        <label htmlFor="validationCustom04" className="form-label">
+          State
+        </label>
+        <select className="form-select" id="validationCustom04" required>
+          <option selected disabled value="">
+            Choose...
+          </option>
+          <option>...</option>
+        </select>
+        <div className="invalid-feedback">Please select a valid state.</div>
+      </div>
+      <div className="col-md-3">
+        <label htmlFor="validationCustom05" className="form-label">
+          Zip
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="validationCustom05"
+          required
+        />
+        <div className="invalid-feedback">Please provide a valid zip.</div>
+      </div>
+      <div className="col-12">
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="invalidCheck"
+            required
+          />
+          <label className="form-check-label" htmlFor="invalidCheck">
+            Agree to terms and conditions
+          </label>
+          <div className="invalid-feedback">
+            You must agree before submitting.
+          </div>
+        </div>
+      </div>
+      <div className="col-12">
+        <button className="btn btn-primary" type="submit">
+          Submit form
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
